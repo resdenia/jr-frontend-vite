18b805bb2fdecef2c981982b0fdcc87399c9233b
AntonKolomiiets <anton.kolomiiets@logz.io>
2022-10-04 16:19:27 +0300

    UPDATE PAGE

--- src/components/AdminComponents/Select/index.jsx
+++ src/components/AdminComponents/Select/index.jsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
-const Select = ({ options, onClick }) => {
-    return <div></div>;
+const renderOptions = (options, onSet, setVisibleSelect) => {
+    return options.map((option, id) => {
+        return (
+            <div
+                onClick={() => {
+                    onSet(option.value);
+                    setVisibleSelect(false);
+                }}
+                className={styles.iconWrapper}
+                key={`${key}_${id}`}
+            >
+                {option.name}
+            </div>
+        );
+    });
+};
+
+const Select = ({ options, onClick, label }) => {
+    const [preview, setPreview] = useState(null);
+    const [visibleSelect, setVisibleSelect] = useState(false);
+    const ref = useRef(null);
+
+    const handleClickOutside = (event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+            setVisibleSelect(false);
+        }
+    };
+    useEffect(() => {
+        document.addEventListener('click', handleClickOutside, true);
+        return () => {
+            document.removeEventListener('click', handleClickOutside, true);
+        };
+    }, []);
+    const handlerOpenSelect = () => {
+        setVisibleSelect(true);
+    };
+    return (
+        <>
+            <div className={styles.selectIconWrapper}>
+                {preview ? <div className={styles.preview}>{preview}</div> : ''}
+            </div>
+            <div className={styles.buttonAdd} onClick={handlerOpenSelect}>
+                Select {label}
+            </div>
+
+            {visibleSelect ? (
+                <div className={styles.listDropDown} ref={ref}>
+                    <h3>{label}</h3>
+                    <div className={styles.iconContainer}>
+                        {renderOptions(options, onClick, setVisibleSelect)}
+                    </div>
+                </div>
+            ) : (
+                ''
+            )}
+        </>
+    );
 };
 
 export default Select;
--- src/components/DashboardElements/AddExpence/index.jsx
+++ src/components/DashboardElements/AddExpence/index.jsx
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react';
+import { UserContext } from '../../../context/user/userContext';
 import styles from './AddExpence.module.css';
 import Input from '../../Input';
 import Button from '../../AdminComponents/Button';
 import DailyRange from '../DailyRange';
 import Select from '../../AdminComponents/Select';
 const AddExpence = ({ onSubmitHandler }) => {
+    const { wallets, typeOfExpence } = useContext(UserContext);
     const datePicker = (data) => {
         console.log(data);
     };
@@ -19,6 +21,8 @@ const AddExpence = ({ onSubmitHandler }) => {
                 }}
             >
                 <p>Choose Wallet</p>
+                <Select />
+                <Select />
                 <p>Choose Type</p>
 
                 <DailyRange
